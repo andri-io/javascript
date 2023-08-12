654b64b5db2b1115796a7b708ebe07b7c011a20b
andri-oc <a92231374@gmail.com>
2023-08-12 14:31:47 +0800

    pure funtion

---
+++ functional-programming/pure-funtion.js
@@ -0,0 +1,21 @@
+// impure function
+let PI = 3.14;
+
+const luasLingkaran = (jejari) => {
+    return PI * (jejari * jejari);
+}
+
+console.log (`Luas Lingkaran : ${luasLingkaran(2)}`);
+PI = 5;
+console.log (`Luas Lingkaran : ${luasLingkaran(2)}`);
+
+
+// pure Function
+const hitungLuasLingkaran = (jariJari) =>{
+    return 3.14 * (jariJari * jariJari);
+}
+
+
+console.log(`Luas Lingkarang : ${hitungLuasLingkaran(4)}`);
+console.log(`Luas Lingkarang : ${hitungLuasLingkaran(5)}`);
+console.log(`Luas Lingkarang : ${hitungLuasLingkaran(6)}`);
