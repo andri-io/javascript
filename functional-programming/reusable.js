8824cb05ab9cdd0f039ba77a6544321e04221753
andri-oc <a92231374@gmail.com>
2023-08-13 13:57:19 +0800

    functional programming

--- functional-programming/reusable.js
+++ functional-programming/reusable.js
@@ -1,7 +1,7 @@
 // Array Map
 
 const newArray = ['Arif','Rahmadani','Zet Erlangga','Gati'].map((name) => {return `${name}`});
-console.table(newArray);
+console.info('Map',newArray);
 
 //Array filter
 
@@ -28,7 +28,53 @@ const students = [
     }
 ];
 
-const eligibleForScholarshipStudent = students.filter((student) => student.score >= 80);
-console.info(eligibleForScholarshipStudent);
+
+// Array Filter
+const eligibleForScholarshipStudent = students.filter(student => student.score >= 80);
+console.info('Filter : ',eligibleForScholarshipStudent);
+// Array Reduce
 const totalScore = students.reduce((acc,curr) => acc + curr.score, 0);
-console.info('Total Score : s',totalScore);
+console.info('Reduce  :',totalScore);
+// Array Find
+const findBayhaqi = students.find(student => student.name === 'Bayhaqi');
+console.info('Find : ',findBayhaqi);
+
+
+// Array Sort 
+const months = ["March", "Jan", "Feb", "Dec"];
+months.sort();
+console.log(months);
+
+// compire number
+const num = [1,5,3,6,7,2,7,8,9,];
+
+const compireNumber = (a, b) => {
+    return a - b;
+}
+const sorting = num.sort(compireNumber);
+console.log(sorting);
+
+// Array Every
+const scores = [80,85,90];
+const minimumScores = 60;
+
+const result = scores.every(score => score >= minimumScores);
+console.log(result);
+
+// Array ForEach 
+const user = ['Bayhaqi','Eos','Arif','Zet','Rahmadani'];
+
+user.forEach(name => console.log(name));
+
+
+
+
+
+
+
+
+
+
+
+
+
