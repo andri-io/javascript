1046e9d653de36bfcd424f1c01cfa9a95361b6d2
andri-oc <a92231374@gmail.com>
2023-08-14 16:04:49 +0800

    switch cae statement

---
+++ fundamental/switchCase.js
@@ -0,0 +1,40 @@
+let language = "French";
+let greeting = null;
+
+switch (language) {
+  case "English":
+    greeting = "Good Morning!";
+    break;
+  case "French":
+    greeting = "Bonjour!";
+    break;
+  case "Japanese":
+    greeting = "Ohayou Gozaimasu!";
+    break;
+  default:
+    greeting = "Selamat Pagi!";
+}
+
+console.log(greeting);
+
+// Pemilihan Grade Berdasarkan Nilai:   
+let score = 75;
+let grade;
+switch (true) {
+  case score >= 90:
+    grade = "A";
+    break;
+  case score >= 80:
+    grade = "B";
+    break;
+  case score >= 70:
+    grade = "C";
+    break;
+  case score >= 60:
+    grade = "D";
+    break;
+  default:
+    grade = "F";
+}
+console.log(`Your grade: ${grade}`);
+
