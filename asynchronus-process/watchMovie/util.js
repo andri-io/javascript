d57eedac6b3aae0e665768a2ecc0dd9e28e944a7
andri-oc <a92231374@gmail.com>
2023-08-15 11:31:45 +0800

    Asyn Await

---
+++ asynchronus-process/watchMovie/util.js
@@ -0,0 +1,43 @@
+function withDrawMoney(amount){
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            if(amount > 100){
+                reject(new Error('Not enough money to withdraw'));
+            }
+            resolve(amount);
+        },1000);
+    });
+}
+
+function buyCinemaTicket(money){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            if(money < 10){
+                reject(new Error("not enough money to buy ticket"));
+            }
+            resolve('Ticket-1');
+        },1000);
+    });
+}
+
+function goInsideCinema(ticket){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(!ticket){
+                reject(new Error('No Ticket'));
+            }
+
+            resolve('Enjoy The Movie');
+        },1000)
+    })
+}
+
+module.exports = {withDrawMoney, buyCinemaTicket, goInsideCinema};
+
+
+
+
+
+
+
+
